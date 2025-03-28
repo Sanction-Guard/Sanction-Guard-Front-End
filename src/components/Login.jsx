f1bae5d869868f35c5dfb52df7587d1ced2d42a7
Kisara Herath <kisara.20231922@iit.ac.lk>
2025-03-28 19:46:41 +0530

    updated localhost

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -41,7 +41,7 @@ const Login = () => {
     setError('');
 
     try {
-      const response = await fetch('http://localhost:3001/api/auth/login', {
+      const response = await fetch('http://54.197.3.150:3001/api/auth/login', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
